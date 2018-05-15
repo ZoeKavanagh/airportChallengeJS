1ab671be4648bb6486f91c95b1ec8ebc41f5f36e
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-05-15 15:24:24 +0100

    created plane land and takeoff functions

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -7,7 +7,7 @@ describe("Airport", function() {
 
   describe("planes", function() {
     it("is created with no planes", function () {
-      expect(airport.planes).toEqual([])
+      expect(airport.planes).toEqual([]);
     });
   });
 
@@ -15,7 +15,25 @@ describe("Airport", function() {
     it("is expected to land planes", function() {
       var flyingPlane = jasmine.createSpy('flyingPlane');
       airport.land(flyingPlane);
-      expect(airport.planes).toContain(flyingPlane)
+      expect(airport.planes).toContain(flyingPlane);
     });
   });
+
+  describe("takeOff", function() {
+
+    var landedPlane = jasmine.createSpy('landedPlane');
+
+    it("is expected to return a taking off plane", function() {
+      airport.planes = [landedPlane];
+      expect(airport.takeOff(landedPlane)).toEqual(landedPlane);
+    });
+
+    it("is expected to no longer contain plane after takeOff", function() {
+      airport.planes = [landedPlane];
+      airport.takeOff(landedPlane);
+      expect(airport.planes).toEqual([]);
+    });
+  });
+
+
 });
--- src/Airport.js
+++ src/Airport.js
@@ -5,3 +5,11 @@ function Airport() {
 Airport.prototype.land = function(plane) {
   this.planes.push(plane);
 };
+
+Airport.prototype.takeOff = function(plane) {
+  return this.planes.splice(this._planeIndex(plane), 1)[0];
+};
+
+Airport.prototype._planeIndex = function(plane) {
+  var index = this.planes.indexOf(plane);
+};
