23da08f75a57ad9e72ef416444303a04c0f6f71b
William Powell <william.powell92@gmail.com>
2018-05-15 16:04:41 +0100

    Created stormy weather spy and guard statement for taking off

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -21,19 +21,42 @@ describe("Airport", function() {
 
   describe("takeOff", function() {
 
-    var landedPlane = jasmine.createSpy('landedPlane');
+    var landedPlane = jasmine.createSpy("landedPlane");
+    var otherLandedPlane = jasmine.createSpy("otherLandedPlane");
+    var weather;
+
+    beforeEach(function() {
+      weather = {
+        isStormy: function() {}
+      };
+    });
 
     it("is expected to return a taking off plane", function() {
+      spyOn(weather, "isStormy").and.returnValue(false);
       airport.planes = [landedPlane];
-      expect(airport.takeOff(landedPlane)).toEqual(landedPlane);
+      expect(airport.takeOff(landedPlane, weather)).toEqual(landedPlane);
     });
 
     it("is expected to no longer contain plane after takeOff", function() {
+      spyOn(weather, "isStormy").and.returnValue(false);
       airport.planes = [landedPlane];
-      airport.takeOff(landedPlane);
+      airport.takeOff(landedPlane, weather);
       expect(airport.planes).toEqual([]);
     });
+
+    it("still contains one plane after another takes off", function() {
+      spyOn(weather, "isStormy").and.returnValue(false);
+      airport.planes = [landedPlane, otherLandedPlane];
+      airport.takeOff(otherLandedPlane, weather);
+      expect(airport.planes).toEqual([landedPlane])
+    });
+
+    it("will not take off planes in stormy weather", function() {
+      spyOn(weather, "isStormy").and.returnValue(true);
+      airport.planes = [landedPlane];
+      expect(function() {
+        airport.takeOff(landedPlane, weather);
+      }).toThrowError("It's too stormy!");
+    });
   });
-
-
 });
--- src/Airport.js
+++ src/Airport.js
@@ -6,10 +6,18 @@ Airport.prototype.land = function(plane) {
   this.planes.push(plane);
 };
 
-Airport.prototype.takeOff = function(plane) {
+Airport.prototype.takeOff = function(plane, weather) {
+  if (weather.isStormy()) {
+    throw new Error("It's too stormy!");
+  }
+
   return this.planes.splice(this._planeIndex(plane), 1)[0];
 };
 
 Airport.prototype._planeIndex = function(plane) {
-  var index = this.planes.indexOf(plane);
+  return this.planes.indexOf(plane);
 };
+
+// if (!this.isFlying()) {
+//   throw new Error("Plane already landed");
+// }
